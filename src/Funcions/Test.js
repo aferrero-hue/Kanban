c732558055574f3ece7fbeadf2d1487658b87ef6
aferrero-hue <66164925+aferrero-hue@users.noreply.github.com>
2023-11-17 18:04:17 +0100

    Cambis amb Drag & Drop

--- src/Funcions/Test.js
+++ src/Funcions/Test.js
@@ -29,31 +29,18 @@ function del(){
 }
 
 //Les seguents probes permeten arrastrar elements en DIV's
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 
-function dropHandler(ev) {
-    console.log("Fichero(s) arrastrados");
-  
-    // Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
-      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-        // Si los elementos arrastrados no son ficheros, rechazarlos
-        if (ev.dataTransfer.items[i].kind === "file") {
-          var file = ev.dataTransfer.items[i].getAsFile();
-          console.log("... file[" + i + "].name = " + file.name);
-        }
-      }
-    } else {
-      // Usar la interfaz DataTransfer para acceder a el/los archivos
-      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-        console.log(
-          "... file[" + i + "].name = " + ev.dataTransfer.files[i].name,
-        );
-      }
-    }
-  
-    // Pasar el evento a removeDragData para limpiar
-    removeDragData(ev);
-  }
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+//Exemple 2:
