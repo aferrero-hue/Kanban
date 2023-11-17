6232d98d31b675039a6c802d046b66818a9d4b27
aferrero-hue <66164925+aferrero-hue@users.noreply.github.com>
2023-11-17 17:42:31 +0100

    Simple Commit

--- src/Funcions/Test.js
+++ src/Funcions/Test.js
@@ -1,3 +1,9 @@
+//Aquest fitxer ens permet fer probes.
+//De moment compté el codi com a exemple per modificar elements.
+//Funcionament:
+//Permet crear y borrar elements en el localstorage
+//Crea una llista
+
 const ul = document.querySelector('ul');
 const input = document.getElementById('item');
 let itemsArray = localStorage.getItem('items') ?
@@ -16,9 +22,38 @@ function add(){
   addTask(input.value);
   input.value = '';
 }
-
 function del(){
   localStorage.clear();
   ul.innerHTML = '';
   itemsArray = [];
 }
+
+//Les seguents probes permeten arrastrar elements en DIV's
+
+function dropHandler(ev) {
+    console.log("Fichero(s) arrastrados");
+  
+    // Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // Si los elementos arrastrados no son ficheros, rechazarlos
+        if (ev.dataTransfer.items[i].kind === "file") {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          console.log("... file[" + i + "].name = " + file.name);
+        }
+      }
+    } else {
+      // Usar la interfaz DataTransfer para acceder a el/los archivos
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        console.log(
+          "... file[" + i + "].name = " + ev.dataTransfer.files[i].name,
+        );
+      }
+    }
+  
+    // Pasar el evento a removeDragData para limpiar
+    removeDragData(ev);
+  }
