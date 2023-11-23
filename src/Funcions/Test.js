b29afa9cd131dd51051c7c17e436d450211e7ee2
aferrero-hue <66164925+aferrero-hue@users.noreply.github.com>
2023-11-23 15:10:35 +0100

    Tester

--- src/Funcions/Test.js
+++ src/Funcions/Test.js
@@ -4,14 +4,29 @@
 //Permet crear y borrar elements en el localstorage
 //Crea una llista
 
+//Dades que s'han de agafar:
+/*
+Codi identificador unic.
+Descripció.
+Data automatica de la creació?
+Data prevista per la finalització
+Responsable
+Prioritat
+*/
+//-------------------------
+
+//Agafar informació 
 const ul = document.querySelector('ul');
 const input = document.getElementById('item');
+
+
+//Agafar informació del Local Storage
 let itemsArray = localStorage.getItem('items') ?
 JSON.parse(localStorage.getItem('items')) : [];
 
 itemsArray.forEach(addTask);
 function addTask(text){
-  const li = document.createElement('li')
+  const li = document.createElement('li')  
   li.textContent = text;
   ul.appendChild(li);
 }
@@ -22,25 +37,23 @@ function add(){
   addTask(input.value);
   input.value = '';
 }
+//Funció que borra pero tot.
 function del(){
   localStorage.clear();
   ul.innerHTML = '';
   itemsArray = [];
+  //alert(typeof(itemsArray));
 }
 
-//Les seguents probes permeten arrastrar elements en DIV's
-function allowDrop(ev) {
-  ev.preventDefault();
+function delsingle(){
+  
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
+//Elements cap a futur:
+//Si volem borrar, s'hauria de assignar una ID a la tasca.
+//Exemple: La ID assignada que correspongui a la mateixa que es mostra.
+ul.setAttribute("id", "IDEXEMPLE");
+//Utilitzarem un array per desar les dades.
+//Per borrar un element individual REF: https://codepen.io/szymongabrek/pen/QMmeyQ
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
 
-//Exemple 2:
